c9a8ef371ff09a881f0d37f20a352aa02585018e
Ian Johnson <enjalot@gmail.com>
2010-11-02 00:26:09 -0400

    preparing to pull in myrnas changes

--- rtps/rtpslib/RTPSettings.h
+++ rtps/rtpslib/RTPSettings.h
@@ -9,12 +9,13 @@ class RTPSettings
 {
 public:
     //decide which system to use
-    enum SysType {Simple, SPH, BOIDS};
+    enum SysType {Simple, SPH, SimpleFlock};
     SysType system;
 
 
     RTPSettings();
     RTPSettings(SysType system, int max_particles, float dt);
+    RTPSettings(int max_particles, float maxspeed, float separationdist, float perceptionrange, float color[]);
 
     //maximum number of particles a system can hold
     int max_particles;
