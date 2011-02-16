e629855e6f7f87077d74f59a239e82da4fdfd88e
Ian Johnson <enjalot@gmail.com>
2011-02-16 01:34:05 -0500

    Hose object made and partially working. still need to calculate rate

--- rtps/rtpslib/domain/IV.cpp
+++ rtps/rtpslib/domain/IV.cpp
@@ -67,4 +67,39 @@ std::vector<float4> addSphere(int num, float4 center, float radius, float spacin
 
 }
 
+
+std::vector<float4> addDisc(int num, float4 center, float4 u, float4 v, float radius, float spacing)
+{
+
+    spacing *= 1.99f; //should probably just figure out whats up with my spacing
+
+    float4 umin = -radius*u;
+    float4 vmin = -radius*v;
+    printf("umin %f %f %f %f\n", umin.x, umin.y, umin.z, umin.w);
+    printf("vmin %f %f %f %f\n", vmin.x, vmin.y, vmin.z, vmin.w);
+
+    std::vector<float4> rvec;
+    int i = 0;
+    float d2 = 0.;
+    float r2 = radius*radius;
+    for(float du = 0.; du < 2.*radius; du += spacing) {
+    for(float dv = 0.; dv < 2.*radius; dv += spacing) {
+        if(i >= num) break;
+        float4 part = center + umin + u*du + vmin + v*dv;
+        part.w = 1.0f;
+        //printf("part %f %f %f %f\n", part.x, part.y, part.z, part.w);
+        d2 = dist_squared(part-center);
+        //printf("d2: %f, r2: %f\n", d2, r2);
+        if(d2 < r2)
+        {
+            rvec.push_back(part);
+            i++;
+        }
+    }
+    }
+    return rvec;
+
+}
+
+
 }
