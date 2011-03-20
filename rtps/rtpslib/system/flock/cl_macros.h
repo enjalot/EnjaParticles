b61e26d386158230d425db0e1fb78c97b696da29
Myrna Merced Serrano <mymese@Myrna-Merced-Serranos-MacBook-Pro.local>
2011-03-20 11:02:57 -0400

    status: the vectors have not been modified inside cl_density, still trying to figure out why, I always get the initial values for all vectors in which I'm storing my rules

--- rtps/rtpslib/system/flock/cl_macros.h
+++ rtps/rtpslib/system/flock/cl_macros.h
@@ -29,16 +29,25 @@
 #define FETCH_POS(t, i) t[i+POS*numParticles]
 
 // change nb of neighbors in GE_FLOCK::setArrays as well
-#define neigh(i, j)  index_neigh[j+50*i] // max of 50 neighbors
+#define neigh(i, j)         index_neigh[j+50*i] // max of 50 neighbors
 
 #define density(i) 			vars_sorted[i+DENS*numParticles].x
 #define den(i) 				vars_sorted[i+DENS*numParticles]	// added by mymese for more generality
 #define surface(i) 			vars_sorted[i+COL*numParticles]
 #define pos(i) 				vars_sorted[i+POS*numParticles]
 #define vel(i) 				vars_sorted[i+VEL*numParticles]
-#define veleval(i)    			vars_sorted[i+VELEVAL*numParticles]
+#define veleval(i)    	    vars_sorted[i+VELEVAL*numParticles]
 #define force(i) 			vars_sorted[i+FOR*numParticles]
-#define xflock(i)    			vars_sorted[i+XFLOCK*numParticles]
+#define xflock(i)    		vars_sorted[i+XFLOCK*numParticles]
+
+// boids macros
+#define acc_separation(i)   force(i)
+#define acc_alignment(i)    surface(i)
+#define acc_cohesion(i)     xflock(i)
+#define flockmates(i)       den(i).x
+#define sep_flockmates(i)   den(i).y
+
+
 // accessing density and color requires two memory access. 
 // Could be more efficient if stored in local point-based array
 /*
@@ -57,7 +66,7 @@
 #define unsorted_surf_tens(i) 		vars_unsorted[i+SURF_TENS   *numParticles]
 #define unsorted_color_normal(i)    vars_unsorted[i+NORMAL      *numParticles]
 #define unsorted_veleval(i)    		vars_unsorted[i+VELEVAL     *numParticles]
-#define unsorted_xflock(i)    		vars_unsorted[i+XFLOCK        *numParticles]
+#define unsorted_xflock(i)    		vars_unsorted[i+XFLOCK      *numParticles]
 
 
 // FOR DEBUGGING (could check for DEBUG flag
